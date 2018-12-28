60d134af10613bfe965784e49fad5c9d77bf181a
ninglin00 <845893576@qq.com>
2018-12-29 00:15:14 +0800

    订单确认页

---
+++ src/service/order-service.js
@@ -0,0 +1,27 @@
+
+var _ajax = require('util/ajax.js');
+
+var _order = {
+    // 获取商品列表
+    getProductList : function(listParam, resolve, reject){
+        _ajax.request({
+            url     : _ajax.getServerUrl('/product/list.do'),
+            data    : listParam,
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取商品详细信息
+    getProductDetail : function(productId, resolve, reject){
+        _ajax.request({
+            url     : _ajax.getServerUrl('/product/detail.do'),
+            data    : {
+                productId : productId
+            },
+            success : resolve,
+            error   : reject
+        });
+    }
+};
+module.exports = _order;
